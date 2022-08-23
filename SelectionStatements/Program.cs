722be49f62545c1559f4a1e5a495b12ab2a57e77
robertsteele7 <atpcycle7@gmail.com>
2022-08-23 10:06:19 -0500

    first commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 13;
+            
+            Console.WriteLine("Guess the Favorite Number");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("You are Correct!!! Here's a COOKIE!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
